238c636c543b99d936d535a09bdbb25bbad84212
Vimal Patel <vp@123insight.com>
2017-01-06 16:19:03 +0530

    working

--- lib/app.rb
+++ lib/app.rb
@@ -5,18 +5,18 @@ require_relative "radar"
 
 class FlightSchool < Sinatra::Base
   get "/" do
-    # erb :index, locals: { airports: ["JFK", "SFO", "OHR", "FWA", "OAK", "SEA", "LAX"] }
+    erb :index, locals: { airports: ["JFK", "SFO", "OHR", "FWA", "OAK", "SEA", "LAX"] }
   end
 
-  # get "/airports/:code" do
-  #   begin
-  #     airport_status = Radar.status_for(params[:code])
-  #     erb :airport, locals: { status: airport_status }
-  #   rescue Radar::NoSuchAirport
-  #     status 404
-  #     erb :no_airport
-  #   end
-  # end
+  get "/airports/:code" do
+    begin
+      airport_status = Radar.status_for(params[:code])
+      erb :airport, locals: { status: airport_status }
+    rescue Radar::NoSuchAirport
+      status 404
+      erb :no_airport
+    end
+  end
 
   get "/api/airports/:code" do
     begin
